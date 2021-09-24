1605810e3e23f7a4c31cb5ba855adffe554a62ca
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-24 03:04:57 +0300

    solved generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,49 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
+//import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <K,V>compare(p1: Pair<K,V>, p2: Pair<K,V>): Boolean {
+    return ((p1.first == p2.first) && (p1.second == p2.second))
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <Any: Comparable<Any>>countGreaterThan(anArray: Array<Any>, elem: Any): Int {
+    return anArray.filter { it > elem }.count()
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<Any: Comparable<Any>> {
+    val list: MutableList<Any> = mutableListOf()
+
+    fun add(value: Any) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
+class Stack<Any> {
+    private val stackListOwn: MutableList<Any> = mutableListOf()
 
+    fun push(elem: Any) {
+        stackListOwn.add(elem)
+    }
+
+    fun pop(): Any {
+        if (isEmpty())
+            return throw IndexOutOfBoundsException("Мы уже всё удалили:(")
+        else {
+            return stackListOwn.removeLast()
+        }
+    }
+
+    fun isEmpty(): Boolean {
+        return stackListOwn.isEmpty()
+    }
+
+    fun size(): Int {
+        return stackListOwn.size
+    }
 }
