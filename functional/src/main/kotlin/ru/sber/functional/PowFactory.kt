c80100325574f93419571689e10acae314a61da2
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-26 20:09:08 +0300

    upd import pow

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,5 +1,6 @@
 package ru.sber.functional
 
+import kotlin.math.pow
 object PowFactory {
     /**
      * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
