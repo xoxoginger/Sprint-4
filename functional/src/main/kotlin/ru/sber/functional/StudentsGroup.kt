ae3db9108503454ff276fd33dc0791a5f4b428ca
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-26 21:17:06 +0300

    solved functional

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,18 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    init {
+        students = listOf(
+            Student(firstName = "Hulia", lastName = "Vysotina", averageRate = 84.7),
+            Student(firstName = "Rick", lastName = "Sanchez", age = 74, averageRate = 93.3),
+            Student(firstName = "Morty", lastName = "Smith", averageRate = 14.6),
+            Student(firstName = "BoJack", lastName = "Horseman", specialization = "actor", averageRate = 59.0),
+            Student(firstName = "Maeve", lastName = "Wiley", city = "Melbrook", averageRate = 86.3),
+            Student(firstName = "Otis", lastName = "Milborn", city = "Melbrook", averageRate = 99.9)
+
+        )
+    }
+    fun filterByPredicate(pred: (Student) -> Boolean): List<Student> {
+        return students.filter(pred)
     }
 }
