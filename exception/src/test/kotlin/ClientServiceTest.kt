81448ca788cbe9ed10f6012053d645eec61c8b2f
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-09-29 22:15:20 +0300

    solving exceptions

--- exception/src/test/kotlin/ClientServiceTest.kt
+++ exception/src/test/kotlin/ClientServiceTest.kt
@@ -18,21 +18,75 @@ class ClientServiceTest {
     }
 
     @Test
-    fun `fail save client - validation error`() {
+    fun `fail save client - phone validation error`() {
         val client = getClientFromJson("/fail/user_with_bad_phone.json")
-        assertThrows<ValidationException>("Ожидаемая ошибка") {
+        val exception = assertFailsWith<ValidationException>("Недопустимый формат номера телефона") {
             clientService.saveClient(client)
         }
+        assertEquals(exception.errorCode[0], ErrorCode.INVALID_PHONE)
+        //assertEquals(exception.errorCode[1], ErrorCode.NULLOREMPTY_VALUE)
+        //assertEquals(exception.errorCode[2], ErrorCode.INVALID_LENGTH)
+    }
+
+    @Test
+    fun `fail save client - first name validation error`() {
+        val client = getClientFromJson("/fail/user_with_bad_first_name.json")
+        val exception = assertFailsWith<ValidationException>("Недопустимый формат имени") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode[0], ErrorCode.INVALID_FIRSTLASTNAME)
+        //assertEquals(exception.errorCode[1], ErrorCode.NULLOREMPTY_VALUE)
+        //assertEquals(exception.errorCode[2], ErrorCode.INVALID_LENGTH)
+    }
+
+    @Test
+    fun `fail save client - last name validation error`() {
+        val client = getClientFromJson("/fail/user_with_bad_last_name.json")
+        val exception = assertFailsWith<ValidationException>("Неверный формат фамилии") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode[0], ErrorCode.INVALID_FIRSTLASTNAME)
+        //assertEquals(exception.errorCode[1], ErrorCode.NULLOREMPTY_VALUE)
+        //assertEquals(exception.errorCode[2], ErrorCode.INVALID_LENGTH)
+    }
+
+    @Test
+    fun `fail save client - email validation error`() {
+        val client = getClientFromJson("fail/user_with_bad_email.json")
+        val exception = assertFailsWith<ValidationException>("Недопустимый формат email-a") {
+            clientService.saveClient(client)
+        }
+        assertEquals(exception.errorCode[0], ErrorCode.INVALID_EMAIL)
+        //assertEquals(exception.errorCode[1], ErrorCode.NULLOREMPTY_VALUE)
+        //assertEquals(exception.errorCode[2], ErrorCode.INVALID_LENGTH)
+    }
+
+    @Test
+    fun `fail save client - snils validation error`() {
+        val client = getClientFromJson("fail/user_with_bad_snils.json")
+        val exception = assertFailsWith<ValidationException>("Недопустимый СНИЛС") {
+            clientService.saveClient(client)
+        }
+        //assertEquals(exception.errorCode[0], ErrorCode.INVALID_SNILS)
+        //assertEquals(exception.errorCode[1], ErrorCode.NULLOREMPTY_VALUE)
+        assertEquals(exception.errorCode[0], ErrorCode.INVALID_LENGTH)
     }
 
     @Test
     fun `fail save client - validation errors`() {
         val client = getClientFromJson("/fail/user_data_corrupted.json")
-        val exception = assertFailsWith<ValidationException>("Ожидаемая ошибка") {
+        val exception = assertFailsWith<ValidationException>("В полях содержатся ошибки") {
             clientService.saveClient(client)
         }
-        assertEquals(exception.errorCode[0], ErrorCode.INVALID_CHARACTER)
+        assertEquals(exception.errorCode[0], ErrorCode.INVALID_PHONE)
+        assertEquals(exception.errorCode[1], ErrorCode.NULLOREMPTY_VALUE)
+        assertEquals(exception.errorCode[2], ErrorCode.INVALID_LENGTH)
+        assertEquals(exception.errorCode[3], ErrorCode.INVALID_EMAIL)
+        assertEquals(exception.errorCode[4], ErrorCode.INVALID_SNILS)
+        //assertEquals(exception.errorCode[5], ErrorCode.INVALID_FIRSTLASTNAME)
     }
+
+
 
     private fun getClientFromJson(fileName: String): Client = this::class.java.getResource(fileName)
         .takeIf { it != null }
